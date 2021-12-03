bba7a0735922c9df903f73b464c88e655d2dedce
Husam Almanakly <husam@Husams-Macbook.fios-router.home>
2021-12-02 20:58:51 -0500

    Initial database schema + authentication

--- Final Project/backend/auth.py
+++ Final Project/backend/auth.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 from flask import Blueprint, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
+from .models import User
+from . import db
 
 
 auth = Blueprint('auth', __name__)
@@ -11,9 +15,19 @@ def login():
     if request.method == 'POST': 
         username = request.form['username']
         password = request.form['password']
-        return redirect(url_for('auth.mainReturn', username = username))
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password): 
+            return redirect(url_for('auth.login'))
+
+        login_user(user, remember=False)
+        return redirect(url_for('main.planner'))
     return render_template('auth.html', request_method = request_method)
 
-@auth.route('/planner/<string:username>', methods=['GET', 'POST'])
-def mainReturn(username):
-    return render_template('index.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
--- Final Project/backend/main.py
+++ Final Project/backend/main.py
@@ -1,30 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from backend.models import User
+from flask_login import login_required, current_user 
 
 main = Blueprint("main", __name__)
 
 @main.route('/')
 def root(): 
+    # ali = User(id = 1, username="aghuman", password=generate_password_hash("123suckad", method="sha256"), email="ali.ghuman@cooper.edu", gpa=3.84)
+    # husam = User(id=2, username="halmanakly", password=generate_password_hash("yaryar123", method="sha256"), email="husam.almanakly@cooper.edu", gpa=3.92)
+    # layth = User(id=3, username="lyassin", password=generate_password_hash("smallcock69", method="sha256"), email="layth.yassin@cooper.edu", gpa=3.9)
+
+    # # with app.app_context():
+    # db.session.add(ali)
+    # db.session.add(husam)
+    # db.session.add(layth)
+    # db.session.commit()
     return redirect(url_for('auth.login'))
 
-class User(db.Model):
-    __tablename__ = 'Users'
-    uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    gpa = db.Column(db.Float)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-# @app.route('/')
-# def hello_negro():
-#     return "Hello Negro!!!!"
-# @app.route('/')
-
+@main.route('/planner')
+@login_required
+def planner():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     main.run(debug=True)
